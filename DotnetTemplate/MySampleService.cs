32ac0309222041373ac2244b94c00a9a33dde6c8
Vikash Choudhary <servikash@gmail.com>
2023-07-07 11:47:23 +0530

    Adds unused variables to create a code smells

--- DotnetTemplate/MySampleService.cs
+++ DotnetTemplate/MySampleService.cs
@@ -4,11 +4,16 @@ public static class MySampleService
 {
     public static string MethodOne()
     {
+        string methodName = "Method One";
         return "Output from method one.";
+        int myNum = 15;
+        Console.WriteLine(myNum);
     }
 
     public static string MethodTwo()
     {
+        string firstName = "John";
+        string lastName = "Daniel";
         return "Output from method two.";
     }
 }
